a4205f76c02ae48a731819e98ef4ccab82c75bf7
antoineraulin <antoin.raulin@icloud.com>
2017-01-31 18:59:27 +0100

    update

--- index.js
+++ index.js
@@ -229,3 +229,14 @@ function add(){
             time2 = false;
         }
     }
+
+function showSnackBar() {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar")
+
+    // Add the "show" class to DIV
+    x.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
